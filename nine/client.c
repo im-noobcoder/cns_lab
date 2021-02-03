81a348f46079e7b711515c39ad7472311beb2c6d
im-noobcoder <sachhunaik8585@gmail.com>
2021-02-03 18:07:48 +0530

    UDP client server program update(nine)

--- nine/client.c
+++ nine/client.c
@@ -1,55 +1,28 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-void error(char *);
-int main(int argc, char *argv[])
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#define PORT 8000
+
+int main()
 {
-    int sock, length, n;
-    struct sockaddr_in server, from;
-    struct hostent *hp;
-    char buffer[256];
-    if (argc != 3)
-    {
-        printf("Usage: server port\n");
-        exit(1);
-    }
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0)
-    {
-        error("socket");
-    }
-    server.sin_family = AF_INET;
-    hp = gethostbyname(argv[1]);
-    if (hp == 0)
-    {
-        error("Unknown host");
-    }
-    bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-    server.sin_port = htons(atoi(argv[2]));
-    length = sizeof(struct sockaddr_in);
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-    n = sendto(sock, buffer, strlen(buffer), 0, &server, length);
-    if (n < 0)
-    {
-        error("Sendto");
-    }
-    n = recvfrom(sock, buffer, 256, 0, &from, &length);
-    if (n < 0)
-    {
-        error("recvfrom");
-    }
-    write(1, "Got an ack: ", 12);
-    write(1, buffer, n);
-}
-void error(char *msg)
-{
-    perror(msg);
-    exit(0);
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0), length;
+    struct sockaddr_in serveraddr;
+    length = sizeof(serveraddr);
+    char buffer[1024], message[100];
+
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    
+    printf("enter message : ");
+    scanf("%s", message);
+
+    sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&serveraddr, length);
+    int n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)NULL, NULL);
+    buffer[n] = '\0';
+    printf("got an acknowledgement : %s\n", buffer);
 }
--- nine/server.c
+++ nine/server.c
@@ -1,55 +1,27 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-void error(char *msg)
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#define PORT 8000
+
+int main()
 {
-    perror(msg);
-    exit(0);
-}
-int main(int argc, char *argv[])
-{
-    int sock, length, fromlen, n;
-    struct sockaddr_in server;
-    struct sockaddr_in from;
-    char buf[1024];
-    if (argc < 2)
+    int serverfd = socket(AF_INET, SOCK_DGRAM, 0), length;
+    char buffer[1024], message[] = "got your message";
+    struct sockaddr_in serveraddr, clientaddr;
+    length = sizeof(clientaddr);
+
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bind(serverfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    while(1)
     {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(0);
-    }
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0)
-    {
-        error("Opening socket");
-    }
-    length = sizeof(server);
-    bzero(&server, length);
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(atoi(argv[1]));
-    if (bind(sock, (struct sockaddr *)&server, length) < 0)
-    {
-        error("binding");
-    }
-    fromlen = sizeof(struct sockaddr_in);
-    while (1)
-    {
-        n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&from, &fromlen);
-        if (n < 0)
-        {
-            error("recvfrom");
-        }
-        write(1, "Received a datagram: ", 21);
-        write(1, buf, n);
-        n = sendto(sock, "Got your message\n", 17,
-                   0, (struct sockaddr *)&from, fromlen);
-        if (n < 0)
-        {
-            error("sendto");
-        }
+        int n = recvfrom(serverfd, buffer, 1024, 0, (struct sockaddr *)&clientaddr, (socklen_t*)&length);
+        buffer[n] = '\0';
+        printf("from client : %s\n", buffer);
+        sendto(serverfd, message, strlen(message), 0, (struct sockaddr*)&clientaddr, length);
     }
 }
